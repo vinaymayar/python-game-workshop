d198fa95c9280b7061629d8e7710835295ec49b7
Jenny <jennyl@snupi.com>
2016-01-18 00:40:42 -0200

    Adding lesson2

---
+++ lesson2/exercises.py
@@ -0,0 +1,107 @@
+# We can operate on integers in Python.
+# The syntax for simple expressions is:
+#   <number> <operator> <number>
+#
+# We can perform basic arithmetic with numbers
+#   Add         10 + 2
+#   Subtract    10 - 2
+#   Multiply    10 * 2
+#   Divide      10 / 2
+
+# Exercise 1:
+#   Perform all basic arithmetic operators on 20 and 4 and print the results.
+
+# Exercise 2:
+#   Perform all basic arithmetic operators on 10 and 4 and print the results.
+
+# We can also operate on floating point numbers in Python.
+# A floating point number is a number that contains a fractional part.
+#   Example 1:    1.0 + 1
+#   Result 1:     2.0
+#
+#   Example 2:    1.5 + 1
+#   Result 2:     2.5
+
+# Exercise 3:
+#   Perform all basic arithmetic operators on 10 and 4.0 and print the results.
+#   Do you notice a difference from the results in Exercise 2?
+
+# Similar to arithmetic in real life, we can use parentheses to denote subcomputations.
+#   Example:    (10 + 2) * 4
+#   Result:     48
+
+# Exercise 4:
+#   Calculate 8 + (5 / 2 * 10) - 3 by hand.  What is your answer?
+#   Compute 8 + (5 / 2 * 10) - 3 in Python.  Is this the answer you expected?
+
+# Exercise 5:
+#   Compute 8 + (5 / 2 * 10.0) - 3 in Python.  Is this the answer you expected?
+
+# Exercise 6:
+#   Compute 8 + (5 / 2.0 * 10) - 3 in Python.  Is this the answer you expected?
+
+# We can also perform more complicated arithmetic.
+#
+# The modulo operator, or %, computes the remainder of the integer division of two numbers.
+# For example:
+#   5 % 2 = 1
+#
+# The modulo operator is useful for:
+#   * Determining whether a number is even or odd
+#   * Finding multiples of numbers
+#   * Getting the last digit of a number
+
+# Exercise 7:
+#   Using the modulo operator, determine how to distinguish between even and odd numbers.
+
+# Exercise 8:
+#   Using the modulo operator, determine if 3849381 is a multiple of 7.
+
+# Exercise 9:
+#   Using the modulo operator, print the last digit of 12345.
+
+# We can compute powers of numbers
+#   5 ** 2 represents 5 raised to the power of 2, or 5^2
+
+# Exercise 10:
+#   Compute and print 10^2.
+
+# Exercise 11:
+#   Compute and print 10.5^2.
+
+# Exercise 12:
+#   Compute and print 10^2.5.
+
+# BONUS EXERCISES
+# Exercise 13:
+#   Using only integers, write an equation with answer 50
+
+# Exercise 14:
+#   Using only integers, write an equation with answer 1000
+
+# Exercise 15:
+#   Using integers and floats, write an equation with answer 50.0
+
+# Exercise 16:
+#   Using integers and floats, write an equation with answer 1000.0
+
+# Exercise 17:
+#   Write an equation with answer 50.5
+
+# Exercise 18:
+#   Write an equation with answer 1000.5
+
+# Exercise 19:
+#   Write an equation with answer 50 that uses the modulo operator
+
+# Exercise 20:
+#   Write an equation with answer 1000 that uses the modulo operator
+
+# Exercise 21:
+#   Write an equation with answer 50.0 that uses the exponent operator
+
+# Exercise 22:
+#   Write an equation with answer 1000.0 that uses the exponent operator
+
+# Exercise 23:
+#   Write an expression to get the second to last digit of 12345.
